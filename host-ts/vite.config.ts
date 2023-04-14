58dba946377f192b1262bedbb9e6e0c877b3704e
Ajay Shrestha <er.ajaystha@gmail.com>
2023-04-14 09:13:27 -0400

    vite configuration

--- host-ts/vite.config.ts
+++ host-ts/vite.config.ts
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import federation from "@originjs/vite-plugin-federation";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    federation({
+      name: "host",
+      remotes: {
+        remoteApp: "http://localhost:3000/assets/remoteEntry.js",
+      },
+      shared: ["react", "react-dom"],
+      exposes: {},
+    }),
+  ],
+  build: {
+    modulePreload: false,
+    target: "esnext",
+    minify: false,
+    cssCodeSplit: false,
+  },
+});
---
+++ remote-ts/src/Header.tsx
@@ -0,0 +1,9 @@
+export const Header = () => {
+  return (
+    <div className="p-5 bg-red-500 text-white text-3xl font-bold">
+      Header from Remote II
+    </div>
+  );
+};
+
+export default Header;
